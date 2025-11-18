062fd292239a5d6fa879dd49b8b7c255f0c21f37
Yuan Lukito <yuanlukito@gmail.com>
2025-11-18 08:55:13 +0700

    tambahan logo ukdw

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title('Ini akan live di streamlit.app')
 
-st.image('tiukdw.jpg')
+col1, col2 = st.columns()
+with col1:
+    st.image('tiukdw.jpg', width=100)
+with col2:
+    st.image('logo-ukdw.png', width=100)
