b3542949bb07515a0a4917c98b73b12e0f091edf
Yuan Lukito <yuanlukito@gmail.com>
2025-11-18 08:56:17 +0700

    jumlah kolom = 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Ini akan live di streamlit.app')
 
-col1, col2 = st.columns()
+col1, col2 = st.columns(2)
 with col1:
     st.image('tiukdw.jpg', width=100)
 with col2:
