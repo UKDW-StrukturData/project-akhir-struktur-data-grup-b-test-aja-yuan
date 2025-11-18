a451785f25bfdbaacf4aa420261d2f80a63c97c2
Yuan Lukito <yuanlukito@gmail.com>
2025-11-18 08:57:38 +0700

    tambahan deskripsi aplikasi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title('Ini akan live di streamlit.app')
+st.text('Ini adalah aplikasi yang digunakan untuk demo deployment ke streamlit.app')
 
 col1, col2 = st.columns(2)
 with col1:
